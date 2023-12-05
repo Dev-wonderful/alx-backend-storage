75f58cb982d2bdecb9ffc4efe888a55596fafa2c
Dev-wonderful <adeonederful20@gmail.com>
2023-12-05 13:56:01 +0000

    redis check

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""A redis exercise module"""
+import redis
+from uuid import uuid4
+from typing import Union
+
+
+class Cache:
+    """A cache class"""
+
+    def __init__(self):
+        """initialize a cavhe instance and redis"""
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+
+    def store(self, data: Union[str, int, bytes, float]) -> str:
+        """store a data in redis using a unique identifier"""
+        key: str = str(uuid4())
+        self._redis.set(key, data)
+        return key
